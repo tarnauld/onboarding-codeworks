8217576b103591ce8509594774f992e6fc261140
Timothée Arnauld <timotheearnauld@mbp-de-timothee.home>
2022-05-23 17:49:18 +0200

    :sparkles: Upload file using VueJS, tiny-emmiter and axios

---
+++ frontend/src/components/purchases/Purchases.vue
@@ -0,0 +1,21 @@
+<script>
+import emitter from 'tiny-emitter/instance';
+
+export default {
+    data: () => {
+        return {
+            basic: []
+        }
+    },
+    methods: {
+        addFile() {
+            emitter.emit('add-file-event', this.basic[0]);
+        }
+    }
+}
+</script>
+<template>
+    <va-file-upload v-model="basic" dropzone type="single" file-types=".csv" @file-added="addFile()" />
+</template>
+<style>
+</style>
--- frontend/src/views/PurchasesView.vue
+++ frontend/src/views/PurchasesView.vue
@@ -1,17 +1,31 @@
 <script>
+import Purchases from '../components/purchases/Purchases.vue'
+import Upload from '../components/purchases/Upload.vue'
+import Output from '../components/purchases/Output.vue'
+
+export default {
+    components: {
+        Purchases,
+        Upload,
+        Output
+    }
+}
 </script>
 <template>
-<div class="purchases">
+    <div class="purchases">
         <h1>Purchases</h1>
-        </div>
+        <Purchases/>
+        <Upload/>
+        <Output/>
+    </div>
 </template>
 <style>
- @media (min-width: 1024px) {
-        .purchases {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
+@media (min-width: 1024px) {
+    .purchases {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
+}
 </style>
