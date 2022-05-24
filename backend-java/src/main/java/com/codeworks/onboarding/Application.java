156d658f6baa32ca8c8bc4155b080d0bb36adeb3
Timothée Arnauld <timotheearnauld@mbp-de-timothee.home>
2022-05-24 18:05:21 +0200

    :loud_sound: Update logback config

--- backend-java/src/main/java/com/codeworks/onboarding/Application.java
+++ backend-java/src/main/java/com/codeworks/onboarding/Application.java
@@ -1,15 +1,17 @@
 package com.codeworks.onboarding;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Application.class);
+        logger.info("Application started.");
         SpringApplication.run(Application.class, args);
     }
 }
